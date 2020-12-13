7f3f1b88a5156044b36c949cab71f281752581f8
Abuenameh Aiyejina <abuenameh@gmail.com>
2020-12-12 21:13:35 -0400

    Added IPv6 support.

--- root/app/duck.sh
+++ root/app/duck.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/with-contenv bash
 
 . /app/duck.conf
-RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$SUBDOMAINS&token=$TOKEN&ip="`
+IP=$(wget -qO- "http://myexternalip.com/raw")
+IPV6=$(wget -q --output-document - http://checkipv6.dyndns.com/ | grep -o "[0-9a-f\:]\{8,\}")
+RESPONSE=`curl -s "https://www.duckdns.org/update?domains=$SUBDOMAINS&token=$TOKEN&ip=$IP&ipv6=$IPV6"`
 if [ "$RESPONSE" = "OK" ]; then
 echo "Your IP was updated at "$(date)
 else
